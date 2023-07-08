c1e70ed8d02097503a9a72735500b8dd192f538b
unknown <eltaroon1082000@gmail.com>
2023-07-09 02:08:14 +0300

    last

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,37 @@
-404
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>CROPCOPTER</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{asset('web_files')}}/vendor/Bootstrap/css/bootstrap.min.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="{{asset('web_files')}}/vendor/FontAwesome/css/all.min.css">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="{{asset('web_files')}}/css/main.css">
+    <link rel="stylesheet" href="{{asset('web_files')}}/css/error404.css">
+
+  </head>
+
+  <body>
+    <div id="clouds">
+        <div class="cloud x1"></div>
+        <div class="cloud x1_5"></div>
+        <div class="cloud x2"></div>
+        <div class="cloud x3"></div>
+        <div class="cloud x4"></div>
+        <div class="cloud x5"></div>
+    </div>
+    <div class='c'>
+        <div class='_404'>404</div>
+        <hr>
+        <div class='_1'>THE PAGE</div>
+        <div class='_2'>WAS NOT FOUND</div>
+        <a class='btn' href='{{url('')}}'>BACK TO CropCopter</a>
+    </div>'
+</body>
+</html>
